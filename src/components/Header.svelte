3cecf30e0e0cc150b8494352c1d8ee234e6749eb
Clement Yanez <yanezcontact@gmail.com>
2025-03-07 17:45:38 +0100

    wip: header fixed top

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,25 +4,38 @@
 
 <header>
     <div>
-        <p>icone hub</p>
         <ul>
-            <li>premier lien</li>
-            <li>deuxieme lien</li>
-            <li>troisieme lien</li>
-            <li>quatrieme lien</li>
+            <div class="elem"></div>
+            <div class="elem"></div>
+            <div class="elem"></div>
+            <div class="elem"></div>
+            <div class="elem"></div>
         </ul>
     </div>
 </header>
 
 <style>
     div{
+        position: fixed;
+        top: 0;
+        z-index: 999;
         width: 100%;
         height: 100px;
-        background-color: lightblue;
-        margin-bottom: 25px;
     }
 
     ul{
+        width: 100%;
+        height: 100%;
         display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 0 20%;
+
+        .elem{
+            position: relative;
+            height: 50px;
+            width: 50px;
+            background-color: white;
+        }
     }
 </style>
